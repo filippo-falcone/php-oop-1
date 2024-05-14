7afde1782670b3677a3971c972f397a75a051699
filippo-falcone <falconefilippo98@hotmail.com>
2024-05-14 16:41:12 +0200

    feat: added Main

--- index.php
+++ index.php
@@ -37,6 +37,27 @@ require_once __DIR__ . '/db.php';
             </div>
         </nav>
     </header>
+    <main>
+        <div class="container w-75 py-4">
+            <div class="row row-cols-md-2 row-cols-lg-3 g-3 justify-content-center">
+                <?php foreach ($movies as $movie) { ?>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo $movie->title; ?></h5>
+                                <h6 class="card-subtitle mb-2 text-muted"><?php echo $movie->director; ?></h6>
+                                <p class="card-text"><?php echo $movie->year; ?></p>
+                                <p class="card-text"><?php echo $movie->country; ?></p>
+                                <p class="card-text"><?php echo implode(', ', $movie->genre); ?></p>
+                                <p class="card-text"><?php echo $movie->duration; ?></p>
+                                <p class="card-text"><?php echo $movie->rating; ?></p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
